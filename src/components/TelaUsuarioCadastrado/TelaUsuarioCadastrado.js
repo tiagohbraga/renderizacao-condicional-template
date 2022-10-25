1aa0dc844033c41be4c8d068d36c72fd48e95d7e
Tiago Braga <tiagohbraga@gmail.com>
2022-10-25 12:00:19 -0300

    trocando de paginas atravez de botões

---
+++ src/components/TelaUsuarioCadastrado/TelaUsuarioCadastrado.js
@@ -0,0 +1,18 @@
+import { Div, Titulo } from "./styled";
+import TelaCadastro from "../TelaCadastro/TelaCadastro";
+import TelaLogin from "../TelaLogin/TelaLogin"
+import { FormContainer } from "../TelaLogin/styled";
+
+
+function TelaUsuarioCadastrado() {
+    return (
+
+        <Div>
+            <Titulo>Você concluiu o cadastro, parabéns!!!</Titulo>
+
+        </Div>
+
+    )
+}
+
+export default TelaUsuarioCadastrado;
